7f044e2e95ee7d194a62f093ba829be2d2236811
Steve <alucard263096@126.com>
2018-04-05 01:58:56 +0800

    commit

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,4 +1,4 @@
-import { AppBase } from "../../app/AppBase"; 
+import { AppBase } from "../../app/AppBase";
 import { AlbumApi } from '../../apis/album.api';
 
 class Content extends AppBase {
@@ -16,20 +16,20 @@ class Content extends AppBase {
     var that = this;
     super.onShow();
   }
-  goupload(){
-    var that=this;
+  goupload() {
+    var that = this;
     wx.showActionSheet({
       itemList: ['照片', '视频'],
       success: function (res) {
         if (!res.cancel) {
-          if(res.tapIndex==0){
-            that.Base.takeImage("memberphoto",(path)=>{
+          if (res.tapIndex == 0) {
+            that.Base.takeImage("memberphoto", (path) => {
               var api = new AlbumApi();
               api.upload({
-                content:path,
-                filetype:"P"
-              },(ret)=>{
-                if(ret.code=="0"){
+                content: path,
+                filetype: "P"
+              }, (ret) => {
+                if (ret.code == "0") {
                   wx.showToast({
                     title: '图片上传成功',
                   })
@@ -56,10 +56,44 @@ class Content extends AppBase {
       }
     });
   }
+  clickPhoto() {
+    var that = this;
+    that.Base.takeImage("memberphoto", (path) => {
+      var api = new AlbumApi();
+      api.upload({
+        content: path,
+        filetype: "P"
+      }, (ret) => {
+        if (ret.code == "0") {
+          wx.showToast({
+            title: ret.return,
+          })
+        }
+      });
+    });
+  }
+  clickVideo() {
+    var that = this;
+    that.Base.takeVideo("memberphoto", (path) => {
+      var api = new AlbumApi();
+      api.upload({
+        content: path,
+        filetype: "V"
+      }, (ret) => {
+        if (ret.code == "0") {
+          wx.showToast({
+            title: ret.return,
+          })
+        }
+      });
+    });
+  }
 }
 var page = new Content();
 var body = page.generateBodyJson();
 body.onLoad = page.onLoad;
 body.onShow = page.onShow;
 body.goupload = page.goupload;
+body.clickPhoto = page.clickPhoto;
+body.clickVideo = page.clickVideo;
 Page(body)
